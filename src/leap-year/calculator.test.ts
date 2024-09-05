3354fe3eb7c83209683857156d8d9431e5ddd37d
Mark Bradley <braddle@gmail.com>
2024-09-05 15:35:36 +0100

    Adding new test file

--- src/leap-year/calculator.test.ts
+++ src/leap-year/calculator.test.ts
@@ -5,7 +5,3 @@
 //
 // For example, 2001 is a typical common year and 1996 is a typical leap year, whereas 1900 is an atypical common year
 // and 2000 is an atypical leap year.
-
-describe('Leap Year Calculator', () => {
-
-});
