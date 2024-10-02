fe6d795029cdca6247c652bd9cac43ff3b1792af
Mark Bradley <braddle@gmail.com>
2024-10-02 11:57:15 +0100

    Frist Kata - Leap year calculator

--- src/leap-year/calculator.test.ts
+++ src/leap-year/calculator.test.ts
@@ -5,3 +5,36 @@
 //
 // For example, 2001 is a typical common year and 1996 is a typical leap year, whereas 1900 is an atypical common year
 // and 2000 is an atypical leap year.
+
+function calculator(year) {
+    function isTypicalLeapYear() {
+        return year % 4 == 0;
+    }
+
+    if (isTypicalLeapYear()) {
+        return true;
+    }
+
+    return false;
+}
+
+describe('Leap Year Calculator', () => {
+    describe('Typical Leap Years', () => {
+        it('should be true the year 4', () => {
+            expect(calculator(4)).toBeTruthy()
+        });
+
+        it('should be true for the year 8', () => {
+            expect(calculator(8)).toBeTruthy()
+        });
+
+        it('should be true for the year 1996', () => {
+            expect(calculator(1996)).toBeTruthy()
+        });
+    });
+
+    it('should be false for the year 3', () => {
+        expect(calculator(3)).toBeFalsy()
+    });
+
+});
